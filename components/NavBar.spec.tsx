17981c3da1fc9c3e44e696ec2124cb8d8b335163
Riya Dattani <riyaddattani@gmail.com>
2019-11-28 15:22:37 +0000

    fix navbar spec

--- components/NavBar.spec.tsx
+++ components/NavBar.spec.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { render } from "@testing-library/react"
 import "@testing-library/jest-dom/extend-expect"
-import NavBar from "./Navbar"
+import NavBar from "./NavBar"
 
 describe("Navigation Bar", () => {
   it("should include the navigation links", () => {
